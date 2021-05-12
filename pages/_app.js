92cfc40ecdd27466f80f47421c944b4b554fca85
reallymimi <lilili846244@outlook.com>
2021-05-12 15:12:27 +0800

    12/5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Navbar, Container, Nav } from "react-bootstrap";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Brand href="#">NAILNAIL</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/pricing">Pricing</Nav.Link>
+
+              <Nav.Link href="/kits">Kits</Nav.Link>
+              <Nav.Link href="/news">News</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Navbar.Collapse>
+            <Navbar.Text>USD</Navbar.Text>
+            <Navbar.Text>Search</Navbar.Text>
+            <Navbar.Text>ACC</Navbar.Text>
+            <Navbar.Text>Shop Car</Navbar.Text>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
